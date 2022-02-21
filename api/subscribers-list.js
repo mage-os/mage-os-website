70d03a484e498d065f940d6972a3aacb137da0b6
Bartek Igielski <bartek.igielski@snow.dog>
2022-02-21 12:05:11 +0100

    Add basic error handling

--- api/subscribers-list.js
+++ api/subscribers-list.js
@@ -2,24 +2,30 @@ import got from 'got'
 import md5 from 'md5'
 
 export default async (request, response) => {
-  const apiKey = process.env.MAILERLITE_API_KEY
+  try {
+    const apiKey = process.env.MAILERLITE_API_KEY
 
-  const raw = await got({
-    method: 'GET',
-    url: `https://api.mailerlite.com/api/v2/groups/${request.query.groupId}/subscribers/active?limit=5000`,
-    headers: {
-      'X-MailerLite-ApiKey': apiKey,
-    }
-  }).json()
+    const raw = await got({
+      method: 'GET',
+      url: `https://api.mailerlite.com/api/v2/groups/${request.query.groupId}/subscribers/active?limit=5000`,
+      headers: {
+        'X-MailerLite-ApiKey': apiKey,
+      }
+    }).json()
 
-  const subsribers = raw
-    .map(subscriber => ({
-      avatar: `https://www.gravatar.com/avatar/${md5(subscriber.email.toLowerCase())}`,
-      name: subscriber.name,
-      company: subscriber.fields.find(({ key }) => key === 'company')?.value || null
-    }))
-    .reverse()
+    const subsribers = raw
+      .map(subscriber => ({
+        avatar: `https://www.gravatar.com/avatar/${md5(subscriber.email.toLowerCase())}`,
+        name: subscriber.name,
+        company: subscriber.fields.find(({ key }) => key === 'company')?.value || null
+      }))
+      .reverse()
 
-  response.setHeader('Cache-Control', 's-maxage=60, stale-while-revalidate')
-  response.json(subsribers)
+    response.setHeader('Cache-Control', 's-maxage=60, stale-while-revalidate')
+    response.json(subsribers)
+
+  }
+  catch (error) {
+    response.status(500).json(error)
+  }
 }
