bd94aeece83ecb63940c40b65eef1af78a2f512c
Bartek Igielski <igloczek@gmail.com>
2022-02-23 00:14:31 +0100

    Update to Astro 0.23 and adjust related parts

--- inline-styles.mjs
+++ inline-styles.mjs
@@ -1,20 +1,36 @@
-import fs from 'fs/promises'
+import path from 'node:path'
+import fs from 'node:fs/promises'
 import { globby } from 'globby'
 
-const root = await fs.readFile('./dist/index.html', 'utf-8')
-const stylesPath = './dist/' + root.match(/<link id="inline-styles" rel="stylesheet" href="(\S+)"/)[1]
-const styles = await fs.readFile(stylesPath, 'utf-8')
-
 const files = await globby('./dist/**/index.html')
+
 await Promise.all(
-  files.map(async path => {
-    const file = await fs.readFile(path, 'utf-8')
-    await fs.writeFile(
-      path,
-      file.replace(
-        /<link id="inline-styles".+type="text\/css">/,
-        `<style>${styles}</style>`
-      )
+  files.map(async htmlPath => {
+    const pageStyles = []
+    const stylesPaths = []
+
+    let file = await fs.readFile(htmlPath, 'utf-8')
+
+    file = file.replace(
+      /<link rel="stylesheet" href="(.*?)">/g,
+      (match, p1) => {
+        stylesPaths.push(p1)
+        return ''
+      }
     )
+
+    await Promise.all(
+      stylesPaths.map(async stylesPath => {
+        const styles = await fs.readFile(path.resolve(path.dirname(htmlPath), stylesPath), 'utf-8')
+        pageStyles.push(styles)
+      })
+    )
+
+    file = file.replace(
+      '<meta charset="UTF-8">',
+      `<style>${pageStyles.join(' ')}</style>\n<meta charset="UTF-8">`
+    )
+
+    await fs.writeFile(htmlPath, file)
   })
 )
